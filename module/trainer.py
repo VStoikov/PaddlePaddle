e126ac1c519c70a0afbc0cc0f6d5279da8e92754
VStoikov <ferarito4@gmail.com>
2020-01-20 00:24:16 +0200

    Added source code

---
+++ module/trainer.py
@@ -0,0 +1,244 @@
+"""
+Trainer class.
+"""
+
+from __future__ import print_function
+
+import logging
+logging.basicConfig(#filename='paddle.log', 
+        #filemode='w',
+        format='[%(levelname)s] %(message)s',)
+
+import os
+import argparse
+import paddle
+import paddle.fluid as fluid
+import numpy
+import sys
+from datetime import datetime
+from model.vgg import vgg_bn_drop
+from model.resnet import resnet_cifar10
+
+class Trainer(object):
+
+    @classmethod
+    def add_cmdline_argument(cls, parser):
+        """ Add the cmdline arguments of trainer. """
+        group = parser.add_argument_group("Trainer")
+
+        group.add_argument(
+            '--infer_network', type=str, default='resnet', help="Set inference network. Default is Resnet. [resnet, vgg]")
+        group.add_argument(
+            '--num_epochs', type=int, default=1, help='Number of epoch.')
+        group.add_argument(
+            '--batch_size', type=int, default=128, help="Batch size.")
+        group.add_argument(
+            '-c', '--enable_ce', action='store_true', help='If set, run the task with continuous evaluation logs.')
+        group.add_argument(
+            '--logger', type=str, default='', help='Path to log data generated in deep learning tasks.')
+        return group
+
+    def __init__(self, hparams):
+        # Use data distributed
+        self.infer_network = hparams.infer_network
+        self.num_epochs = hparams.num_epochs
+        self.batch_size = hparams.batch_size
+        self.enable_ce = hparams.enable_ce
+        self.logger = hparams.logger
+
+        if self.logger:
+            from visualdl import LogWriter
+            self.log_writer = LogWriter(self.logger, sync_cycle=20)
+            # Create two ScalarWriter instances, whose mode is set to be "train"
+            with self.log_writer.mode("train") as logger:
+                self.train_cost = logger.scalar("cost")
+                self.train_acc = logger.scalar("acc")
+
+            # Create a ScalarWriter instance, whose mode is set to be "test"
+            with self.log_writer.mode("test") as logger:
+                self.test_loss = logger.scalar("loss")
+                self.test_acc = logger.scalar("acc")
+
+        #if not os.path.exists(self.save_dir):
+        #    os.makedirs(self.save_dir)
+
+    def inference_network(self):
+        # The image is 32 * 32 with RGB representation.
+        data_shape = [None, 3, 32, 32]
+        images = fluid.data(name='pixel', shape=data_shape, dtype='float32')
+
+        if self.infer_network is 'resnet':
+            predict = resnet_cifar10(images, 32)
+        elif self.infer_network is 'vgg':
+            predict = vgg_bn_drop(images)
+        else:
+            logging.error('The following inference network is not supported! Choose on of: resnet, vgg.')
+        return predict
+
+
+    def train_network(self, predict):
+        label = fluid.data(name='label', shape=[None, 1], dtype='int64')
+        cost = fluid.layers.cross_entropy(input=predict, label=label)
+        avg_cost = fluid.layers.mean(cost)
+        accuracy = fluid.layers.accuracy(input=predict, label=label)
+        return [avg_cost, accuracy]
+
+
+    def optimizer_program(self):
+        return fluid.optimizer.Adam(learning_rate=0.001)
+
+    def train(self, use_cuda, params_dirname):
+        train_start = datetime.utcnow()
+        place = fluid.CUDAPlace(0) if use_cuda else fluid.CPUPlace()
+
+        if self.enable_ce:
+            train_reader = paddle.batch(
+                paddle.dataset.cifar.train10(), batch_size=self.batch_size)
+            test_reader = paddle.batch(
+                paddle.dataset.cifar.test10(), batch_size=self.batch_size)
+        else:
+            test_reader = paddle.batch(
+                paddle.dataset.cifar.test10(), batch_size=self.batch_size)
+            train_reader = paddle.batch(
+                paddle.reader.shuffle(
+                    paddle.dataset.cifar.train10(), buf_size=128 * 100),
+                batch_size=self.batch_size)
+
+        feed_order = ['pixel', 'label']
+
+        main_program = fluid.default_main_program()
+        start_program = fluid.default_startup_program()
+
+        if self.enable_ce:
+            main_program.random_seed = 90
+            start_program.random_seed = 90
+
+        predict = self.inference_network()
+        avg_cost, acc = self.train_network(predict)
+
+        # Test program
+        test_program = main_program.clone(for_test=True)
+        optimizer = self.optimizer_program()
+        optimizer.minimize(avg_cost)
+
+        exe = fluid.Executor(place)
+
+        EPOCH_NUM = self.num_epochs
+
+        # For training test cost
+        def train_test(program, reader):
+            count = 0
+            feed_var_list = [
+                program.global_block().var(var_name) for var_name in feed_order
+            ]
+            feeder_test = fluid.DataFeeder(feed_list=feed_var_list, place=place)
+            test_exe = fluid.Executor(place)
+            accumulated = len([avg_cost, acc]) * [0]
+            for tid, test_data in enumerate(reader()):
+                avg_cost_np = test_exe.run(
+                    program=program,
+                    feed=feeder_test.feed(test_data),
+                    fetch_list=[avg_cost, acc])
+                accumulated = [
+                    x[0] + x[1][0] for x in zip(accumulated, avg_cost_np)
+                ]
+                count += 1
+            return [x / count for x in accumulated]
+
+        # main train loop.
+        def train_loop():
+            feed_var_list_loop = [
+                main_program.global_block().var(var_name) for var_name in feed_order
+            ]
+            feeder = fluid.DataFeeder(feed_list=feed_var_list_loop, place=place)
+            exe.run(start_program)
+
+            step = 0
+            for pass_id in range(EPOCH_NUM):
+                for step_id, data_train in enumerate(train_reader()):
+                    avg_loss_value = exe.run(
+                        main_program,
+                        feed=feeder.feed(data_train),
+                        fetch_list=[avg_cost, acc])
+                    if self.logger is not '' and self.logger is not None:
+                        self.train_cost.add_record(step_id, avg_loss_value[0])
+                        self.train_acc.add_record(step_id, avg_loss_value[1])
+                    if step_id % 100 == 0:
+                        print("\nPass %d, Batch %d, Cost %f, Acc %f" % (
+                            step_id, pass_id, avg_loss_value[0], avg_loss_value[1]))
+                    else:
+                        sys.stdout.write('.')
+                        sys.stdout.flush()
+                    step += 1
+
+                avg_cost_test, accuracy_test = train_test(
+                    test_program, reader=test_reader)
+                train_end = datetime.utcnow()
+                elapsed_time = train_end - train_start
+                if self.logger is not '' and self.logger is not None:
+                    self.test_loss.add_record(pass_id, avg_cost_test)
+                    self.test_acc.add_record(pass_id, accuracy_test)
+                print('\nTest with Pass {0}, Loss {1:2.2}, Acc {2:2.2}'.format(
+                    pass_id, avg_cost_test, accuracy_test))
+                print('Train started at {}'.format(train_start.strftime('%Y-%m-%d %H:%M:%S.%f')))
+                print('Train ended at {}'.format(train_end.strftime('%Y-%m-%d %H:%M:%S.%f')))
+                print('Elapsed time for training is {}'.format(elapsed_time))
+
+                if params_dirname is not None:
+                    fluid.io.save_inference_model(params_dirname, ["pixel"],
+                                                  [predict], exe)
+
+                if self.enable_ce and pass_id == EPOCH_NUM - 1:
+                    print("kpis\ttrain_cost\t%f" % avg_loss_value[0])
+                    print("kpis\ttrain_acc\t%f" % avg_loss_value[1])
+                    print("kpis\ttest_cost\t%f" % avg_cost_test)
+                    print("kpis\ttest_acc\t%f" % accuracy_test)
+
+        train_loop()
+
+    def infer(self, use_cuda, params_dirname, image):
+        from PIL import Image
+        place = fluid.CUDAPlace(0) if use_cuda else fluid.CPUPlace()
+        exe = fluid.Executor(place)
+        inference_scope = fluid.core.Scope()
+        image_path=image
+
+        def load_image(infer_file):
+            im = Image.open(infer_file)
+            im = im.resize((32, 32), Image.ANTIALIAS)
+
+            im = numpy.array(im).astype(numpy.float32)
+            # The storage order of the loaded image is W(width),
+            # H(height), C(channel). PaddlePaddle requires
+            # the CHW order, so transpose them.
+            im = im.transpose((2, 0, 1))  # CHW
+            im = im / 255.0
+
+            # Add one dimension to mimic the list format.
+            im = numpy.expand_dims(im, axis=0)
+            return im
+
+        img = load_image(image_path)
+
+        with fluid.scope_guard(inference_scope):
+            # Use fluid.io.load_inference_model to obtain the inference program desc,
+            # the feed_target_names (the names of variables that will be feeded
+            # data using feed operators), and the fetch_targets (variables that
+            # we want to obtain data from using fetch operators).
+            [inference_program, feed_target_names,
+             fetch_targets] = fluid.io.load_inference_model(params_dirname, exe)
+
+            # Construct feed as a dictionary of {feed_target_name: feed_target_data}
+            # and results will contain a list of data corresponding to fetch_targets.
+            results = exe.run(
+                inference_program,
+                feed={feed_target_names[0]: img},
+                fetch_list=fetch_targets)
+
+            # infer label
+            label_list = [
+                "airplane", "automobile", "bird", "cat", "deer", "dog", "frog",
+                "horse", "ship", "truck"
+            ]
+
+            print("infer results: %s" % label_list[numpy.argmax(results[0])])
---
+++ run.py
@@ -0,0 +1,69 @@
+"""
+Main run function.
+"""
+
+from __future__ import print_function
+
+import logging
+logging.basicConfig(#filename='paddle.log', 
+        #filemode='w',
+        format='[%(levelname)s] %(message)s',)
+
+import argparse
+import os
+import sys
+import paddle.fluid as fluid
+from module.trainer import Trainer
+
+def parse_args():
+    parser = argparse.ArgumentParser("image_classification")
+    parser.add_argument(
+        '-t', '--train', 
+        action='store_true', 
+        help='Whether to run trainning.')
+    parser.add_argument(
+        "-i", '--infer',
+        action='store_true',
+        help="Whether to run inference on the test dataset.")
+    parser.add_argument(
+        '--model_path', type=str, default='', required=True, help="Model storage path.")
+    parser.add_argument(
+        '-g', '--use_cuda', 
+        action='store_true', 
+        help='Choose, if you want to run training with GPU performance.')
+    parser.add_argument(
+        '--image', type=str, default='', help='Path to image.')
+    Trainer.add_cmdline_argument(parser)
+    args = parser.parse_args()
+
+    if len(sys.argv)<=1:
+        parser.print_help(sys.stderr)
+        sys.exit(1)
+
+    return args
+
+def main(use_cuda):
+    if use_cuda and not fluid.core.is_compiled_with_cuda():
+        logging.warning('Your PC is not support CUDA!')
+        return
+
+    trainer = Trainer(args)
+
+    if args.train:
+        trainer.train(use_cuda, args.model_path)
+    elif args.infer:
+        cur_dir = os.path.dirname(os.path.realpath(__file__))
+        path = cur_dir + args.image
+        trainer.infer(use_cuda, args.model_path, path)
+    else:
+        pass
+
+if __name__ == "__main__":
+    # On default, the training runs on CPU
+    args = parse_args()
+    use_cuda = 0
+
+    if args.use_cuda:
+        use_cuda = 1
+
+    main(use_cuda)
